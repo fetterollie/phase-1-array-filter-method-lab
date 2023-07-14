b300025cc35f0a6e51d8f9a53ae8e872fd95884c
Jonathan <jonathan.fetterolf@gmail.com>
2023-07-14 11:32:52 -0400

    complete, all test passing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers = [], name = "") {
+    const matching = drivers.filter(driver => driver.toUpperCase() == name.toUpperCase())
+    return matching
+};
+
+function fuzzyMatch(drivers = [], name = "") {
+    const matching = drivers.filter(driver => driver[0] == name[0])
+    return matching
+};
+
+function matchName(drivers = [], string = "") {
+    const matching = drivers.filter(driver => driver.name == string)
+    return matching
+};
